258c9366f488fed6932f42cef844cd83bcc61284
Cristiano Cortezia <cristiano@britecore.com>
2019-09-28 12:52:55 -0300

    Add initial implementation

---
+++ clockrange/core.py
@@ -0,0 +1,160 @@
+# Standard Library Imports
+import functools
+import math
+from dataclasses import dataclass
+from typing import Any, Iterable, Iterator, List, Tuple, Union
+
+CountLimit = int
+
+CountLimits = Tuple[CountLimit, ...]
+
+ClockSpec = Union[List[Any], range, int]
+
+ClockSpecs = Tuple[ClockSpec, ...]
+
+
+@dataclass
+class CountState:
+    counters: Tuple[int, ...]
+    cycles: int
+
+
+@dataclass
+class ClockState(CountState):
+    rendered: Tuple[Any, ...]
+
+    @classmethod
+    def from_countstate(cls, specs: ClockSpecs, state: CountState) -> "ClockState":
+        rendered = tuple([_rendered_from_counter(s, c) for s, c in zip(specs, state.counters)])
+        return cls(counters=state.counters, cycles=state.cycles, rendered=rendered)
+
+
+class ClockRange:
+    """ClockRange produces values according to a given clock-like periodic sequence specification.
+
+    Parameters
+    ----------
+    specs : ClockSpecs
+
+    See Also
+    ----------
+    clockrange.ClockState : encapsulates values produced by ClockRange instances
+
+    Examples
+    --------
+
+    ClockRange instances are iterable:
+    >>> it = iter(ClockRange((24, 60, 60)))
+
+    ClockRange instances support random access:
+    >>> clock = ClockRange((24, 60, 60))
+    >>> assert clock[60].counters == (0, 1, 0)
+
+    ClockRange specifications accept integers representing 0-N ranges:
+    >>> clock = ClockRange((24, 60, 60))
+    >>> assert clock[0].counters == (0, 0, 0)
+    >>> assert clock[1].counters == (0, 0, 1)
+    >>> assert clock[60].counters == (0, 1, 0)
+    >>> assert clock[86400].counters == (0, 0, 0)
+    >>> assert clock[86400].cycles == 1
+
+    ClockRange specifications accept lists of arbitrary symbols:
+    >>> clock = ClockRange((2, ["A", "B"]))
+    >>> assert clock[0].counters == (0, 0)
+    >>> assert clock[0].rendered == (0, "A")
+    >>> assert clock[3].rendered == (1, "B")
+
+    ClockRange specifications accept native range instances:
+    >>> clock = ClockRange((range(1, 20, 2), 100))
+    >>> assert clock[0].counters == (0, 0)
+    >>> assert clock[0].rendered == (1, 0)
+    >>> assert clock[100].counters == (1, 0)
+    >>> assert clock[100].rendered == (3, 0)
+
+    """
+
+    def __init__(self, specs: ClockSpecs):
+        self.limits = _limits_from_specs(specs)
+        self.specs = specs
+
+    def __getitem__(self, key: int) -> ClockState:
+        if key < 0:
+            raise IndexError("argument 'key' must be >= 0")
+        countstate = _countstate_from_ticks(self.limits, key)
+        return ClockState.from_countstate(self.specs, countstate)
+
+    def __len__(self) -> int:
+        return _multiply(self.limits)
+
+    def __iter__(self) -> Iterator[ClockState]:
+        ticks = 0
+        while True:
+            yield self[ticks]
+            ticks += 1
+
+    def ticks(self, state: ClockState) -> int:
+        """Returns the number of ticks needed to build 'state', according to the known spec
+
+        Parameters
+        ----------
+        state : ClockState
+            ClockState data previously produced by this ClockRange instance
+
+        Returns
+        -------
+        int
+            The number of ticks needed to produce the given state
+        """
+        return _ticks_from_countstate(self.limits, state)
+
+
+def _rendered_from_counter(spec: ClockSpec, counter: int) -> Any:
+    if isinstance(spec, int):
+        return counter
+    if isinstance(spec, (list, tuple)):
+        return spec[counter]
+    if isinstance(spec, range):
+        return spec.start + (counter * spec.step)
+    raise TypeError(f"unsupported type for argument 'spec' {type(spec)}")
+
+
+def _limits_from_specs(specs: ClockSpecs) -> CountLimits:
+    return tuple([_limit_from_spec(spec) for spec in specs])
+
+
+def _limit_from_spec(spec: ClockSpec) -> CountLimit:
+    if isinstance(spec, int):
+        return spec
+    if isinstance(spec, (list, tuple)):
+        return len(spec)
+    if isinstance(spec, range):
+        return math.ceil((spec.stop - spec.start) / spec.step)
+    raise TypeError(f"unsupported type for argument 'spec' {type(spec)}")
+
+
+def _countstate_from_ticks(limits: CountLimits, ticks: int) -> CountState:
+    counters = [0] + [0 for _ in limits]
+    capacity = _multiply(limits)
+    for idx, _ in enumerate(limits):
+        counters[idx] = int(ticks // capacity)
+        ticks = int(ticks % capacity)
+        capacity = int(capacity / limits[idx])
+    cycles = counters[0]
+    counters = counters[1:]
+    counters[-1] = ticks
+    return CountState(tuple(counters), cycles)
+
+
+def _ticks_from_countstate(limits: CountLimits, state: CountState) -> int:
+    ticks = 0
+    counters = [state.cycles] + list(state.counters)
+    capacity = _multiply(limits)
+    for idx, _ in enumerate(limits):
+        ticks += counters[idx] * capacity
+        capacity = int(capacity / limits[idx])
+    ticks += counters[-1]
+    return int(ticks)
+
+
+def _multiply(integers: Iterable[int]) -> int:
+    return functools.reduce(lambda acc, curr: acc * curr, integers, 1)
---
+++ tests/test_clockrange.py
@@ -0,0 +1,80 @@
+# Third-Party Imports
+import pytest
+
+# Local Imports
+from clockrange.core import ClockRange, ClockState, _limit_from_spec, _rendered_from_counter
+
+
+@pytest.mark.parametrize(
+    "specs,ticks,counters,cycles,rendered",
+    (
+        # 1-stage clock
+        ((10,), 0, (0,), 0, (0,)),
+        ((10,), 9, (9,), 0, (9,)),
+        ((10,), 20, (0,), 2, (0,)),
+        ((range(10),), 0, (0,), 0, (0,)),
+        ((range(10),), 9, (9,), 0, (9,)),
+        ((range(10),), 20, (0,), 2, (0,)),
+        ((range(0, 10, 1),), 0, (0,), 0, (0,)),
+        ((range(0, 10, 1),), 9, (9,), 0, (9,)),
+        ((range(0, 10, 1),), 20, (0,), 2, (0,)),
+        ((range(1, 10),), 0, (0,), 0, (1,)),
+        ((range(1, 10),), 9, (0,), 1, (1,)),
+        ((range(1, 10),), 20, (2,), 2, (3,)),
+        ((range(1, 10, 2),), 20, (0,), 4, (1,)),
+        ((["A", "B", "C"],), 0, (0,), 0, ("A",)),
+        ((["A", "B", "C"],), 2, (2,), 0, ("C",)),
+        (([1.22, "B", "C"],), 3, (0,), 1, (1.22,)),
+        # 2-stages clock
+        ((4, 10), 0, (0, 0), 0, (0, 0)),
+        ((4, 10), 10, (1, 0), 0, (1, 0)),
+        ((4, 10), 20, (2, 0), 0, (2, 0)),
+        ((4, 10), 30, (3, 0), 0, (3, 0)),
+        ((4, 10), 40, (0, 0), 1, (0, 0)),
+        ((4, 10), 41, (0, 1), 1, (0, 1)),
+        ((range(4), range(10)), 0, (0, 0), 0, (0, 0)),
+        ((range(4), range(10)), 10, (1, 0), 0, (1, 0)),
+        ((range(4), range(10)), 12, (1, 2), 0, (1, 2)),
+        ((range(4), range(10)), 41, (0, 1), 1, (0, 1)),
+        ((range(1, 4), range(1, 10)), 10, (1, 1), 0, (2, 2)),
+        ((["A", "B", "C"], [10, 20, 30]), 0, (0, 0), 0, ("A", 10)),
+        ((["A", "B", "C"], [10, 20, 30]), 5, (1, 2), 0, ("B", 30)),
+        # 3-stages clock
+        ((2, 4, 10), 41, (1, 0, 1), 0, (1, 0, 1)),
+        ((2, 4, 10), 80, (0, 0, 0), 1, (0, 0, 0)),
+    ),
+)
+def test_clockrange_random_access(specs, ticks, counters, cycles, rendered):
+    assert ClockRange(specs)[ticks] == ClockState(counters, cycles, rendered)
+
+
+def test_clockrange_random_access_with_negative_value_should_raise():
+    crange = ClockRange((4, 10))
+    with pytest.raises(IndexError):
+        crange[-1]
+
+
+def test_clockrange_iterable():
+    iterable = iter(ClockRange((4, 10)))
+    assert next(iterable) == ClockState((0, 0), cycles=0, rendered=(0, 0))
+    assert next(iterable) == ClockState((0, 1), cycles=0, rendered=(0, 1))
+
+
+def test_clockrange_len():
+    assert len(ClockRange((4, 10))) == 40
+
+
+def test_clockrange_state_ticks():
+    crange = ClockRange((4, 10))
+    state = crange[142]
+    assert crange.ticks(state) == 142
+
+
+def test_rendered_from_counter_spec_type_error():
+    with pytest.raises(TypeError):
+        _rendered_from_counter("aaa", None)
+
+
+def test_limit_from_spec_spec_type_error():
+    with pytest.raises(TypeError):
+        _limit_from_spec("aaa")
